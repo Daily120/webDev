5b54c6a7d9edaea1d196a89ee48024b5a87359f8
Daily120 <forzhacker@mail.ru>
2018-08-06 18:08:57 +0200

    Arrays

--- javascriptfeatures/script.js
+++ javascriptfeatures/script.js
@@ -15,3 +15,15 @@ function sing(song) {
     console.log(song);
 }
 sing("This is America. Don't catch you slippin' up");
+
+//Arrays
+var list = ["tiger", "cat", "bear", "bird"];
+var mixed = [1,2, "522", "Helloooooo"];
+mixed.forEach(item => {
+    console.log(item);
+});
+//A list of functions
+var functionList = [function apple() {
+    console.log("apple");
+}]
+functionList[0]();
