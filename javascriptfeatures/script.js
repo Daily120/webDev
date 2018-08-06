6ae1d4a1b50551d94d6734a66817c35e2f08f88a
Daily120 <forzhacker@mail.ru>
2018-08-06 02:31:13 +0200

    very simple javascript

---
+++ javascriptfeatures/script.js
@@ -0,0 +1,17 @@
+// Function Declaration
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
+
+//Anonymous Function () => void
+var sayBye = function() {
+    console.log("Bye");
+}
+sayBye();
+
+//Function with arguments
+function sing(song) {
+    console.log(song);
+}
+sing("This is America. Don't catch you slippin' up");
---
+++ javascriptfeatures/very_simple_staff.js
@@ -0,0 +1,19 @@
+var firstNumber = prompt("Enter first number:");
+var seccondNumber = prompt("Enter seccond number");
+sum = Number(firstNumber) + Number(seccondNumber);
+dif = Number(firstNumber) - Number(seccondNumber);
+mul = Number(firstNumber) * Number(seccondNumber);
+div = Number(firstNumber) / Number(seccondNumber);
+alert("The sum is:" + sum + '\n' +
+    "The difference is:" + dif + '\n' +
+    "The result of multiply is:" + mul + '\n' +
+    "The division result is:" + div);
+
+
+if (false) {
+
+} else if (true) {
+
+} else {
+
+}
