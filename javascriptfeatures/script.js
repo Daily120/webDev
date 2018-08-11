eb217d6af2efe78e647b7eb871c4845a6b35dc8b
Daily120 <forzhacker@mail.ru>
2018-08-12 00:02:04 +0200

    Template strings(.format in python) and function's default arguments

--- javascriptfeatures/script.js
+++ javascriptfeatures/script.js
@@ -120,3 +120,15 @@ const obj3 = {
     b, //b: true
     c  //c: {}
 }
+
+//Template strings
+// `` - tilda key
+const age = 34;
+const pet = "horse"
+
+const greetingBest = `Hello ${name}, you seem to be ${age-10}. What a lovely ${pet} you have!`;
+
+//default arguments
+function greet(name='', age=30, pet='cat') {
+    return `Hello ${name}, you seem to be ${age-10}. What a lovely ${pet} you have!`;
+}
