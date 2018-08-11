bf2da49a1178f42cf0745120bfae89d36a3bdaa1
Daily120 <forzhacker@mail.ru>
2018-08-11 23:12:13 +0200

    ternary operator and switch statement

--- javascriptfeatures/script.js
+++ javascriptfeatures/script.js
@@ -54,3 +54,28 @@ do {
 list.forEach(item => {
     console.log(item);
 })
+
+//ternary operator condition ? expr1 : expr2
+var choise = 5>3 ? true : false;
+
+//switch
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+        case "right":
+            whatHappens = "you found a river";
+            break;
+        case "left":
+            whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction"
+    }
+    return whatHappens;
+}
