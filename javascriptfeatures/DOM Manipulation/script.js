d6ca2479eb3a6c83878562bce951f03553dc9f6c
Daily120 <forzhacker@mail.ru>
2018-08-11 16:12:41 +0200

    Code Refactoring

--- javascriptfeatures/DOM Manipulation/script.js
+++ javascriptfeatures/DOM Manipulation/script.js
@@ -47,20 +47,25 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-    if (input.value) { //input.value.length > 0
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
-    }
-})
+function addItemToTheList() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
 
-input.addEventListener("keypress", function(event){
-    if (input.value && event.key === "Enter") {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+function addItemAfterClick() {
+    if (input.value) { //input.value.length > 0
+        addItemToTheList();
     }
-})
+}
+
+function addItemAfterEnterPress(event) {
+    if (input.value && event.key === "Enter") {
+        addItemToTheList();
+    }
+}
+
+button.addEventListener("click", addItemAfterClick)
+
+input.addEventListener("keypress", addItemAfterEnterPress)
