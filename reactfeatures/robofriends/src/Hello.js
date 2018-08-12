02c8bd581fc038bb5d3cec2d5f5f3bfdedd150b8
Daily120 <forzhacker@mail.ru>
2018-08-13 00:18:26 +0200

    Created my first component

---
+++ reactfeatures/robofriends/src/Hello.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return <h1>Hello React!</h1>
+    }
+}
+
+export default Hello; // We always should export class in order to other files can use it.
