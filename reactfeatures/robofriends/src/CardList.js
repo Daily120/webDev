4ab6a0afc8c92ec7945ba81729445750b8e0166f
Daily120 <forzhacker@mail.ru>
2018-08-13 18:44:13 +0200

    Logo + searchBox design + code refactoring

--- reactfeatures/robofriends/src/CardList.js
+++ reactfeatures/robofriends/src/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
     const cardsArray = robots.map((user) =>{
-        return <Card key={user.id} id={user.id} name={user.name} email= {user.email}/>
+        return <Card key={user.id} id={user.id} name={user.name} email= {user.email}/> //when adding elements through a loop, it needs to have a key prop in order to be recognisible by react
     })
 
     return (
