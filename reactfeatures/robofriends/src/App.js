b5f24b3dc6b343098e24c7681839161dc5f2ef78
Daily120 <forzhacker@mail.ru>
2018-08-13 20:03:36 +0200

    implemented Search. Very important rules of React!

--- reactfeatures/robofriends/src/App.js
+++ reactfeatures/robofriends/src/App.js
@@ -12,16 +12,22 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event);
+    onSearchChange = (event) => { //when you make your own method in react. it should look like this
+        this.setState({ //you always update state using setState function. Not directly! 
+            searchField: event.target.value
+        });
     }
 
     render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={ this.state.robots } />
+                <SearchBox searchChange={ this.onSearchChange }/>
+                <CardList robots={ filteredRobots } />
             </div>
         )
     }
