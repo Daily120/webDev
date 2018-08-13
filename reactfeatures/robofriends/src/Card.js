8cc259baaeddb853b424a45e2d44ab4e3464cb3a
Daily120 <forzhacker@mail.ru>
2018-08-13 17:37:41 +0200

    Destructuring props

--- reactfeatures/robofriends/src/Card.js
+++ reactfeatures/robofriends/src/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 //we can change class to function that returns a view
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${props.id}?200x200`} alt="Robot"/>
+            <img src={`https://robohash.org/${id}?200x200`} alt="Robot"/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
