80ea243b03671738e66597d2314937d720f41e2b
Daily120 <forzhacker@mail.ru>
2018-08-13 17:28:00 +0200

    adding multiple components to a view. + styling with tachyons classes

--- reactfeatures/robofriends/src/Card.js
+++ reactfeatures/robofriends/src/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 //we can change class to function that returns a view
 const Card = () => {
     return (
-        <div>
-            <img src="https://robohash.org/test" alt="Robot"/>
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src="https://robohash.org/test?200x200" alt="Robot"/>
             <div>
                 <h2>Jane Doe</h2>
                 <p>jane.doe@gmail.com</p>
