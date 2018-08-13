501b9fd21a209eb74ea77bcd00bc6ad413fde1d4
Daily120 <forzhacker@mail.ru>
2018-08-13 17:21:49 +0200

    Added a Card Component

---
+++ reactfeatures/robofriends/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//we can change class to function that returns a view
+const Card = () => {
+    return (
+        <div>
+            <img src="" alt="Robot"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
