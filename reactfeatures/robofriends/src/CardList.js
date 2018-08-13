a75090585ab3e0648b586380857c9a647ffdbe52
Daily120 <forzhacker@mail.ru>
2018-08-13 18:20:57 +0200

    CardList refactoring

--- reactfeatures/robofriends/src/CardList.js
+++ reactfeatures/robofriends/src/CardList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    const cardsArray = robots.map((user, index) =>{
-        return <Card id={robots[index].id} name={robots[index].name} email= {robots[index].email}/>
+    const cardsArray = robots.map((user) =>{
+        return <Card id={user.id} name={user.name} email= {user.email}/>
     })
-    
+
     return (
         <div>
             {cardsArray}
