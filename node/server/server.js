45172579ac766fc357bf67e25523ccab28dc1d03
Daily120 <forzhacker@mail.ru>
2018-08-18 17:56:07 +0200

    sending a responce with json + some request information

--- node/server/server.js
+++ node/server/server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log('I hear you! Thanks for the request');
+const server = http.createServer((request, response) => {
+    console.log(request.headers);
+    console.log(request.method);
+    console.log(request.url);
+
+    const user = {
+        name: 'John',
+        hobby: 'Skating'
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 });
 
 server.listen(3000);
