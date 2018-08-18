bbaf3f0904e2749420fb46f4ca791c98327bbfa4
Daily120 <forzhacker@mail.ru>
2018-08-18 19:36:08 +0200

    using body-parser.js to parse body of request

--- node/server/server.js
+++ node/server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 //Express midleware
-app.use((req, res, next) => {
-    console.log("I am called before everyone, when request comes in");
-    next();
-})
+//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); //applies only to Content-Type: application/json
 
 //get request app.post for POST
 app.get('/', (req, res) => {
@@ -15,6 +14,19 @@ app.get('/', (req, res) => {
         hobby: 'soccer'
     }
     res.send(user); //automaticly does JSON.stringify and adds application/json in headers
+});
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body);
+    const user = {
+        name: 'Sally',
+        hobby: 'soccer'
+    }
+    res.send(user);
 });
 
 app.listen(3000);
