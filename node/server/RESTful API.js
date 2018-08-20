92e870d4e545aa712968795f3facf59ad8dc19f5
Daily120 <forzhacker@mail.ru>
2018-08-21 01:00:25 +0200

    Sending static files through server. RESTful api

--- node/server/RESTful API.js
+++ node/server/RESTful API.js
@@ -3,15 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
 
-app.get('/:id', (req, res) => {
-    console.log(req.query); //query string ?name=...&id=....
-    // req.body
-    console.log(req.headers);
-    console.log(req.params); //everything in the link that after / sign, because of :id
-    res.status(404).send("not found");
-})
+// app.get('/:id', (req, res) => {
+//     console.log(req.query); //query string ?name=...&id=....
+//     // req.body
+//     console.log(req.headers);
+//     console.log(req.params); //everything in the link that after / sign, because of :id
+//     res.status(404).send("not found");
+// })
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
