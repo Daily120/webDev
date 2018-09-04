289517539ab97c3769348d49fc93a863ed39773d
Daily120 <forzhacker@mail.ru>
2018-09-05 00:34:44 +0200

    sign in using database

--- reactfeatures/facerecognitionAPI/server.js
+++ reactfeatures/facerecognitionAPI/server.js
@@ -19,38 +19,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors()); //for unsecure requests. just to test the app on a localhsot
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally1234@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[2].email &&
-        bcrypt.compareSync(req.body.password, database.users[2].password)) {
-            res.json(database.users[2]);
+    db.select('email', 'hash').from('login').where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users').where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
         } else {
-            res.status(400).json('error logging in')
+            res.status(400).json('wrong credentials')
         }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
