c1e09b3759ed2e7a440f1feeb74aa4f0c27a7afc
Daily120 <forzhacker@mail.ru>
2018-09-04 23:15:45 +0200

    Added dynamic username and entries to Rank component

--- reactfeatures/facerecognitionapp/src/containers/App/App.js
+++ reactfeatures/facerecognitionapp/src/containers/App/App.js
@@ -111,7 +111,7 @@ class App extends Component {
                 { route === 'home' 
                 ? <div>
                     <Logo />
-                    <Rank />
+                    <Rank userName ={this.state.user.name} entries={this.state.user.entries}/>
                     <ImageLinkForm  onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
                     <FaceRecognition box={box} imageUrl={imageUrl}/>
                   </div>
