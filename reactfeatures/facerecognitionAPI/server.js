5eb7eda5cb6bfed38383f592c49aa2061f4bcba8
Daily120 <forzhacker@mail.ru>
2018-09-05 00:15:23 +0200

    Using transaction when register, cause insert data into 2 tables

--- reactfeatures/facerecognitionAPI/server.js
+++ reactfeatures/facerecognitionAPI/server.js
@@ -56,25 +56,28 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
-    // database.users.push({
-    //     id: '125',
-    //     name: name,
-    //     email: email,
-    //     password: hash,
-    //     entries: 0,
-    //     joined: new Date()
-    // });
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        }).into('login').returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
         })
-        .then(user => {
-            res.json(user[0]);
-        })
-        .catch(err => res.status(400).json('unable to register'))
+        .then(trx.commit) //everything ok? apply chsnges
+        .catch(trx.rollback) //error? rollback changes
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
