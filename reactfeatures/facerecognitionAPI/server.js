52e2aaa3bfff466a5ee30d03bc43610d1608ad02
Daily120 <forzhacker@mail.ru>
2018-09-04 23:05:14 +0200

    Register user into database

--- reactfeatures/facerecognitionAPI/server.js
+++ reactfeatures/facerecognitionAPI/server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
@@ -14,7 +14,7 @@ const postgres = knex({
     }
 })
 
-postgres.select('*').from('users').then(data => {
+db.select('*').from('users').then(data => {
     console.log(data);
 })
 
@@ -60,15 +60,25 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: hash,
-        entries: 0,
-        joined: new Date()
-    });
-    res.json(database.users[database.users.length - 1]);
+    // database.users.push({
+    //     id: '125',
+    //     name: name,
+    //     email: email,
+    //     password: hash,
+    //     entries: 0,
+    //     joined: new Date()
+    // });
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
